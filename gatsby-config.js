f8af5da6a68f7c8d9ab9bd3be429591c98aab2f5
Xeon <nickandreyuser@gmail.com>
2020-01-24 17:43:43 +0100

    Update APIurl in config. Add package-lock.json to .gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.DEPLOY_URL
+            ? 'https://laska-blog-test.herokuapp.com/'
+            : 'http://localhost:1337',
         contentTypes: [
           'article',
           'user'
